3792e4e812323fab2e4346a92434b6380e284283
sandra <you@example.comsandrareichman@gmail.com>
2018-04-01 20:35:32 +0300

    added service and model

---
+++ LibraryData/Migrations/20180331224927_add models2.cs
@@ -0,0 +1,324 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryData.Migrations
+{
+    public partial class addmodels2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckoutHistories_LibraryAssets_LibraryAssetId",
+                table: "CheckoutHistories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Checkouts_LibraryAssets_LibraryAssetId",
+                table: "Checkouts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Holds_LibraryAssets_LibraryAssetId",
+                table: "Holds");
+
+            migrationBuilder.DropTable(
+                name: "LibraryAssets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Videos",
+                table: "Videos");
+
+            migrationBuilder.RenameTable(
+                name: "Videos",
+                newName: "LibraryAssets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Director",
+                table: "LibraryAssets",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "LibraryAssets",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeweyIndex",
+                table: "LibraryAssets",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "LibraryAssets",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfCopies",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "LibraryAssets",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LibraryAssets",
+                table: "LibraryAssets",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryAssets_LocationId",
+                table: "LibraryAssets",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryAssets_StatusId",
+                table: "LibraryAssets",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckoutHistories_LibraryAssets_LibraryAssetId",
+                table: "CheckoutHistories",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Checkouts_LibraryAssets_LibraryAssetId",
+                table: "Checkouts",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Holds_LibraryAssets_LibraryAssetId",
+                table: "Holds",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LibraryAssets_LibraryBranches_LocationId",
+                table: "LibraryAssets",
+                column: "LocationId",
+                principalTable: "LibraryBranches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LibraryAssets_Statuses_StatusId",
+                table: "LibraryAssets",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckoutHistories_LibraryAssets_LibraryAssetId",
+                table: "CheckoutHistories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Checkouts_LibraryAssets_LibraryAssetId",
+                table: "Checkouts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Holds_LibraryAssets_LibraryAssetId",
+                table: "Holds");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LibraryAssets_LibraryBranches_LocationId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LibraryAssets_Statuses_StatusId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LibraryAssets",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LibraryAssets_LocationId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LibraryAssets_StatusId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "DeweyIndex",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfCopies",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "LibraryAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "LibraryAssets");
+
+            migrationBuilder.RenameTable(
+                name: "LibraryAssets",
+                newName: "Videos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Director",
+                table: "Videos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Videos",
+                table: "Videos",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "LibraryAssets",
+                columns: table => new
+                {
+                    Author = table.Column<string>(nullable: true),
+                    DeweyIndex = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true),
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cost = table.Column<decimal>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: false),
+                    LocationId = table.Column<int>(nullable: false),
+                    NumberOfCopies = table.Column<int>(nullable: false),
+                    StatusId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryAssets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibraryAssets_LibraryBranches_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "LibraryBranches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LibraryAssets_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryAssets_LocationId",
+                table: "LibraryAssets",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryAssets_StatusId",
+                table: "LibraryAssets",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckoutHistories_LibraryAssets_LibraryAssetId",
+                table: "CheckoutHistories",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Checkouts_LibraryAssets_LibraryAssetId",
+                table: "Checkouts",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Holds_LibraryAssets_LibraryAssetId",
+                table: "Holds",
+                column: "LibraryAssetId",
+                principalTable: "LibraryAssets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ LibraryData/Models/LibraryBranch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class LibraryBranch
+    {
+        [Required]
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(30,ErrorMessage ="Limit branch name to 30 chars.")]
+        public string Name { get; set; }
+
+
+        
+        [Required]
+        public string address { get; set; }
+
+
+        [Required]
+        public string Telephone { get; set; }
+        public string Description { get; set; }
+        public DateTime OpenDate { get; set; }
+
+
+        public virtual IEnumerable<Patron> Patrons { get; set; }
+        public virtual IEnumerable<LibraryAsset> LibraryAssets { get; set; }
+
+        public virtual string ImageUrl { get; set; }
+
+
+
+
+
+    }
+}
