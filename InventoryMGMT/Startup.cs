3be42782a11779e0026d3c1e0fd6c71527fff278
sandra <you@example.comsandrareichman@gmail.com>
2018-04-02 16:57:45 +0300

    design and start of checkout model

--- InventoryMGMT/Startup.cs
+++ InventoryMGMT/Startup.cs
@@ -27,6 +27,8 @@ namespace InventoryMGMT
             services.AddMvc();
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
+            services.AddScoped<ICheckout, CheckoutService>();
+
             services.AddDbContext<LibraryContext>(options 
                 => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
